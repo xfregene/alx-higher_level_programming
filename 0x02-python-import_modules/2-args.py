9e79ea764d2a40861db8e8f069d035fdb09d2371
Esere Fregene <xfregene@gmail.com>
2022-12-07 14:16:51 -0800

    fixing errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+
+import sys
 
 if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+    i = 1
+    length = len(sys.argv) - 1
+
+    if length == 0:
+        print("{:d} arguments.".format(length))
+    elif length == 1:
+        print("{:d} argument:".format(length))
+    else:
+        print("{:d} arguments:".format(length))
+    while i <= length:
+        print("{:d}: {:s}".format(i, sys.argv[i]))
+        i += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
 if __name__ == "__main__":
     import sys
-    add_arg(sys.argv)
+    i = 1
+    sum = 0
+    length = len(sys.argv) - 1
+    while i <= length:
+        sum += int(sys.argv[i])
+        i += 1
+    print("{}".format(sum))
